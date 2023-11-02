cf736814d29f15d1cd2e0c76ecb73aef04c6a980
hyojinny-k <80536082+hyojinny-k@users.noreply.github.com>
2023-11-02 17:00:59 +0900

    Update student20210777.py

--- HW1/student20210777.py
+++ HW1/student20210777.py
@@ -7,6 +7,7 @@ wr = ws.max_row
 score = []
 sorted_score = []
 rank = [0, 0]
+countF = 0
 
 for r in range(2, wr + 1):
     midterm = ws.cell(row = r, column = 3).value * 0.3
@@ -16,28 +17,30 @@ for r in range(2, wr + 1):
     total = midterm + final + homework + attendance
     ws.cell(row=r, column=7, value=total)
     score.append(total)
+    if total < 40:
+        countF += 1
     r += 1
 
 sorted_score = sorted(score, reverse = True)
 for s in score:
     rank.append(sorted_score.index(s) + 1)
 
-countA = (wr - 1) * 0.3
-countB = (wr - 1) * 0.7
-countC = (wr - 1) - countB
-
+countA = int((wr - 1) * 0.3)
+countB = int((wr - 1) * 0.7) - countA
+countC = (wr - 1) - countA - countB - countF
+#print(countA,countB,countC,countF)
 for r in range(2, wr + 1):
     if ws.cell(row=r, column=7).value < 40:
         ws.cell(row=r, column=8, value='F')
-    elif rank[r] <= countA/2:
+    elif rank[r] <= countA / 2:
         ws.cell(row=r, column=8, value='A+')
     elif rank[r] <= countA:
         ws.cell(row=r, column=8, value='A0')
-    elif rank[r] <= countA+(countB-countA)/2:
+    elif rank[r] <= countA + countB / 2:
         ws.cell(row=r, column=8, value='B+')
-    elif rank[r] <= countB:
+    elif rank[r] <= countA + countB:
         ws.cell(row=r, column=8, value='B0')
-    elif rank[r] <= countB+(countC-countB)/2:
+    elif rank[r] <= countA + countB + countC / 2:
         ws.cell(row=r, column=8, value='C+')
     else:
         ws.cell(row=r, column=8, value='C0')
