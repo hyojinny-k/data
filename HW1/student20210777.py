a45ec17b80ad041e1c5f90c7b77ac0ca576922e8
hyojinny-k <80536082+hyojinny-k@users.noreply.github.com>
2023-11-02 16:40:13 +0900

    Update student20210777.py

--- HW1/student20210777.py
+++ HW1/student20210777.py
@@ -9,9 +9,9 @@ sorted_score = []
 rank = [0, 0]
 
 for r in range(2, wr + 1):
-    midterm = ws.cell(row = r, column = 3).value * 30 / 100
-    final = ws.cell(row = r, column = 4).value * 35 / 100
-    homework = ws.cell(row = r, column = 5).value * 34 / 100
+    midterm = ws.cell(row = r, column = 3).value * 0.3
+    final = ws.cell(row = r, column = 4).value * 0.35
+    homework = ws.cell(row = r, column = 5).value * 0.34
     attendance = ws.cell(row = r, column = 6).value * 1
     total = midterm + final + homework + attendance
     ws.cell(row=r, column=7, value=total)
@@ -32,13 +32,15 @@ for r in range(2, wr + 1):
     elif rank[r] <= countA/2:
         ws.cell(row=r, column=8, value='A+')
     elif rank[r] <= countA:
-        ws.cell(row=r, column=8, value='A')
+        ws.cell(row=r, column=8, value='A0')
     elif rank[r] <= countA+(countB-countA)/2:
         ws.cell(row=r, column=8, value='B+')
+    elif rank[r] <= countB:
+        ws.cell(row=r, column=8, value='B0')
     elif rank[r] <= countB+(countC-countB)/2:
         ws.cell(row=r, column=8, value='C+')
     else:
-        ws.cell(row=r, column=8, value='C')
+        ws.cell(row=r, column=8, value='C0')
 
 wb.save(filename = 'student.xlsx')
 wb.close()
