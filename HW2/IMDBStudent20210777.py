647dca85555959d49631508b9008dcfb0dd7ee6c
hyojinny-k <80536082+hyojinny-k@users.noreply.github.com>
2023-11-08 20:20:18 +0900

    Add files via upload

---
+++ HW2/IMDBStudent20210777.py
@@ -0,0 +1,21 @@
+f1 = open("movies_exp.txt", "rt")
+f2 = open("output.txt", "wt")
+genre = []
+while True:
+    row = f1.readline().replace("\n", "")
+    if not row:
+        break
+    line = row.split('::')
+    if line[-1].find('|') > -1:
+        temp_genre = line[-1].split('|')
+        for g in temp_genre:
+            genre.append(g)
+    else:
+        genre.append(line[-1])
+
+list_genre = list(set(genre))
+for g in list_genre:
+    s = g + ' ' + str(genre.count(g)) + '\n'
+    f2.write(s)
+f1.close()
+f2.close()
