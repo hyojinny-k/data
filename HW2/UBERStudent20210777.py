2d41a444d1d52f851d1961e056aa20adf4669446
hyojinny-k <80536082+hyojinny-k@users.noreply.github.com>
2023-11-09 01:27:07 +0900

    Update UBERStudent20210777.py

--- HW2/UBERStudent20210777.py
+++ HW2/UBERStudent20210777.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from datetime import datetime, date
+from datetime import date
 f1 = open("uber.dat", "rt")
-#f1 = open("uber_exp.txt", "rt")
 f2 = open("uberoutput.txt", "wt")
 days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 result = []
@@ -15,11 +14,20 @@ while True:
     d = int(temp_date[1])
     y = int(temp_date[2])
     day = date(y, m, d).weekday()
-    s = line[0] + ',' + days[day] + ' ' + line[2] + ',' + line[3] + '\n'
-    result.append(s)
+    line[1] = days[day]
+    result.append(line)
 
 result.sort()
+i = 1
 for r in result:
-    f2.write(r)
+    # f2.write(r)
+    if result[i][0] == result[i - 1][0] and result[i][1] == result[i - 1][1]:
+        result[i][2] = int(result[i][2]) + int(result[i-1][2])
+        result[i][3] = int(result[i][3]) + int(result[i-1][3])
+        del result[i-1]
+    else:
+        i += 1
+for r in result:
+    f2.write(r[0] + ',' + r[1] + ' ' + str(r[2]) + ',' + str(r[3]) + '\n')
 f1.close()
 f2.close()
