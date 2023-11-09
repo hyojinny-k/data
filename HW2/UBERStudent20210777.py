f51c29302f6bc7c71025eea23fb470b908f85f87
hyojinny-k <80536082+hyojinny-k@users.noreply.github.com>
2023-11-09 15:21:07 +0900

    Update UBERStudent20210777.py

--- HW2/UBERStudent20210777.py
+++ HW2/UBERStudent20210777.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 import datetime
-
 def getDay(month, date, year):
     r = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
     return r[datetime.date(int(year), int(month), int(date)).weekday()]
-
 
 dic = dict()
 valDic = dict()
 with open("uber.dat", "rt") as f1:
     for line in f1:
         list = line.strip('\n').split(',')
-
         dayList = list[1].split('/')
         key = list[0] + "," + getDay(dayList[0], dayList[1], dayList[2])
 
@@ -22,7 +19,7 @@ with open("uber.dat", "rt") as f1:
             temp1 = int(tList[0]) + int(list[2])
             temp2 = int(tList[1]) + int(list[3])
             dic[key] = str(temp1) + "," + str(temp2)
-
+            
 with open("uberoutput.txt", "wt") as f2:
     for k in dic.keys():
         f2.write(k + " " + dic[k] + "\n")
