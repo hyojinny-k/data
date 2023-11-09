121e8093ab3674648808a127013b314190f0df79
hyojinny-k <80536082+hyojinny-k@users.noreply.github.com>
2023-11-09 14:57:40 +0900

    Update IMDBStudent20210777.py

--- HW2/IMDBStudent20210777.py
+++ HW2/IMDBStudent20210777.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 genre = dict()
-with open("movies_exp.txt", "rt") as f1:
+with open("movie.dat", "rt") as f1:
     for line in f1:
         line.strip('\n')
         genres = line.split('::')
@@ -12,6 +12,6 @@ with open("movies_exp.txt", "rt") as f1:
             else:
                 genre[l] += 1
 
-with open("imdboutputex.txt", "wt") as f2:
+with open("movieoutput.txt", "wt") as f2:
     for k in genre.keys():
         f2.write(k + ' ' + str(genre[k]) + '\n')
